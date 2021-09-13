5645a6bb74fc3429a95ef45369248128715bc539
nicovanniekerk <nico@viretual-academies.com>
2021-09-13 10:40:28 +0200

    UPD: support node version 14.x

--- layers/sharp/build.sh
+++ layers/sharp/build.sh
@@ -1,6 +1,6 @@
 # Variables
 SHARP_VERSION=$(npm show sharp version)
-NODE_VERSION=10.16.3
+NODE_VERSION=14.16.3
 SHARP_DIRECTORY=sharp-$SHARP_VERSION
 TARBALL=sharp-$SHARP_VERSION-aws-lambda-linux-x64-node-$NODE_VERSION.zip
 
